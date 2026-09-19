106e6d11e9fc08c8811622a31dc06f97a1e702bd
unknown <jayshrirathod24@gmail.com>
2026-09-20 00:09:12 +0530

    Build menu Lambda API

--- backend/lambda/orders/handler.py
+++ backend/lambda/orders/handler.py
@@ -1,5 +1,35 @@
+import json
+
+orders = []
+
+
 def lambda_handler(event, context):
-    return {
-        "statusCode": 200,
-        "body": "Orders Lambda is working!"
+
+    order = {
+        "order_id": len(orders) + 1,
+        "customer_name": "Harshad",
+        "items": [
+            {
+                "name": "Vada Pav",
+                "quantity": 2,
+                "price": 20
+            }
+        ],
+        "total": 40,
+        "status": "PLACED"
     }
+
+    orders.append(order)
+
+    return {
+        "statusCode": 201,
+        "headers": {
+            "Content-Type": "application/json"
+        },
+        "body": json.dumps(order)
+    }
+
+
+if __name__ == "__main__":
+    response = lambda_handler({}, {})
+    print(response["body"])
