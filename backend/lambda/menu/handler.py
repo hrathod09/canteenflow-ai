b547ac96716576cd132a7706eebc439bc85ee550
unknown <jayshrirathod24@gmail.com>
2026-09-20 00:05:49 +0530

    Build menu Lambda API

--- backend/lambda/menu/handler.py
+++ backend/lambda/menu/handler.py
@@ -1,10 +1,44 @@
+import json
+
+MENU = [
+    {
+        "id": 1,
+        "name": "Vada Pav",
+        "price": 20,
+        "category": "Snacks"
+    },
+    {
+        "id": 2,
+        "name": "Masala Dosa",
+        "price": 60,
+        "category": "South Indian"
+    },
+    {
+        "id": 3,
+        "name": "Veg Sandwich",
+        "price": 50,
+        "category": "Snacks"
+    },
+    {
+        "id": 4,
+        "name": "Tea",
+        "price": 15,
+        "category": "Beverages"
+    }
+]
+
+
 def lambda_handler(event, context):
+
     return {
         "statusCode": 200,
-        "body": "Menu Lambda is working!"
+        "headers": {
+            "Content-Type": "application/json"
+        },
+        "body": json.dumps(MENU)
     }
 
 
 if __name__ == "__main__":
     response = lambda_handler({}, {})
-    print(response)
+    print(response["body"])
